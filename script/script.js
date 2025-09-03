3b317d5b3852f19599bf974fb7b13fa812e7cb74
Refat al hasan <229121686+refatalhasan@users.noreply.github.com>
2025-09-04 00:09:43 +0600

    home page created and login page updated

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// add event listener
+
+document.getElementById('login-btn').addEventListener('click', function (event) {
+
+    // prevent defult reload
+    event.preventDefault();
+    // console.log('login btn clicked');
+
+    // get the phone number
+    const phoneNumber = document.getElementById('mobile').value;
+    // console.log(phoneNumber);
+
+    // get the pin number
+    const pinNumber = document.getElementById('pin').value;
+    // console.log(pinNumber);
+
+    if (phoneNumber === '123456789' && pinNumber === '1234') {
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Login failed, wrong phone or pin number');
+    }
+})
