578f12be74796c33160650cc3bd2e18d60cf468d
Refat al hasan <229121686+refatalhasan@users.noreply.github.com>
2025-09-03 23:09:27 +0600

    event listner and prevent defult added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('js is connected');
+
+// add event listener
+
+document.getElementById('login-btn').addEventListener ('click', function(event){
+
+    // prevent defult reload
+    event.preventDefault();
+    console.log('login btn clicked');
+
+    // get the phone number from form
+    const phoneNumber = document.getElementById('mobile').value;
+    console.log(phoneNumber)
+    
+})
